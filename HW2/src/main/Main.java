b75be58bf0fdf7c6f8dc1134311765effd8fda99
ror4b7 <ror4b7@gmail.com>
2024-02-09 17:30:10 -0600

    file path fix

--- HW2/src/main/Main.java
+++ HW2/src/main/Main.java
@@ -6,7 +6,7 @@ public static void main(String args[])
   {
       StudentManager manager = new StudentManager(); 
 
-      manager.readFromFile("studentData.txt");
+      manager.readFromFile("../studentData.txt");
 
       //manager.printStudent();
 
